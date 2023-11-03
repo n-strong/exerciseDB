9ab32d8d3b123d691978a37af32cf81765b2619e
Noah Strong <stronoah@oregonstate.edu>
2023-11-02 21:38:52 -0700

    built models file, html form, and added ORM. not currently functional

--- project/models.py
+++ project/models.py
@@ -1,67 +1,51 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+db = SQLAlchemy()
 
-# Define the Workouts table
-class Workouts(Base):
+class Workouts(db.Model):
     __tablename__ = 'Workouts'
+    workout_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    workout_name = db.Column(db.String(50), nullable=False)
+    calorie_count = db.Column(db.BigInteger, nullable=False)
 
-    workout_id = Column(Integer, primary_key=True, autoincrement=True)
-    workout_name = Column(String(50), nullable=False)
-    calorie_count = Column(Integer, nullable=False)
-
-# Define the Exercise table
-class Exercise(Base):
+class Exercise(db.Model):
     __tablename__ = 'Exercise'
 
-    exercise_id = Column(String(50), primary_key=True)
-    exercise_name = Column(String(50), nullable=False)
+    exercise_id = db.Column(db.String(50), primary_key=True)
+    exercise_name = db.Column(db.String(50), nullable=False)
+   
+    equipment = relationship('Equipment', secondary=Exercises_have_Equipment, back_populates="exercises")
 
-# Define the Category table
-class Category(Base):
+class Category(db.Model):
     __tablename__ = 'Category'
 
-    category_id = Column(Integer, primary_key=True, autoincrement=True)
-    category_name = Column(String(50))
+    category_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    category_name = db.Column(db.String(50))
 
-# Define the Equipment table
-class Equipment(Base):
+class Equipment(db.Model):
     __tablename__ = 'Equipment'
 
-    equipment_id = Column(String(50), primary_key=True)
-    equipment_name = Column(String(50), nullable=False)
+    equipment_id = db.Column(db.String(50), primary_key=True)
+   
+    equipment_name = db.Column(db.String(50), nullable=False)
+    secondary=(Exercises_have_Equipment, back_populates="equipment")
 
-# Define the Workouts_have_Exercises association table
-Workouts_have_Exercises = Table(
-    'Workouts_have_Exercises', Base.metadata,
-    Column('workout_id', Integer, ForeignKey('Workouts.workout_id')),
-    Column('exercise_id', String(50), ForeignKey('Exercise.exercise_id')),
+Workouts_have_Exercises = db.Table(
+    'Workouts_have_Exercises', db.Model.metadata,
+    db.Column('workout_id', db.Integer, db.ForeignKey('Workouts.workout_id')),
+    db.Column('exercise_id', db.String(50), db.ForeignKey('Exercise.exercise_id'))
 )
 
-# Define the Exercises_have_Equipment association table
-Exercises_have_Equipment = Table(
-    'Exercises_have_Equipment', Base.metadata,
-    Column('equipment_id', String(50), ForeignKey('Equipment.equipment_id')),
-    Column('exercise_id', String(50), ForeignKey('Exercise.exercise_id')),
+Exercises_have_Equipment = db.Table(
+    'Exercises_have_Equipment', db.Model.metadata,
+    db.Column('equipment_id', db.String(50), db.ForeignKey('Equipment.equipment_id')),
+    db.Column('exercise_id', db.String(50), db.ForeignKey('Exercise.exercise_id'))
 )
 
-# Define the Exercises_have_Categories association table
-Exercises_have_Categories = Table(
-    'Exercises_have_Categories', Base.metadata,
-    Column('exercise_id', String(50), ForeignKey('Exercise.exercise_id')),
-    Column('category_id', Integer, ForeignKey('Category.category_id')),
+Exercises_have_Categories = db.Table(
+    'Exercises_have_Categories', db.Model.metadata,
+    db.Column('exercise_id', db.String(50), db.ForeignKey('Exercise.exercise_id')),
+    db.Column('category_id', db.Integer, db.ForeignKey('Category.category_id'))
 )
 
-# You can create a database engine like this:
-# engine = create_engine('mysql://your_username:your_password@localhost/your_database')
-
-# To use these models, create an SQLAlchemy session and interact with the database.
-# Example of creating a new Workout:
-# from sqlalchemy.orm import sessionmaker
-# Session = sessionmaker(bind=engine)
-# session = Session()
-# new_workout = Workouts(workout_name="Sample Workout", calorie_count=500)
-# session.add(new_workout)
-# session.commit()
--- project/routes.py
+++ project/routes.py
@@ -1,30 +1,43 @@
 '''
 File to support entities.
 '''
-from flask import Flask, render_template
-import mysql.connector
-# from database import db_connect
+from flask import Flask, render_template, jsonify, request
 
+from models import Workouts, Exercise, Equipment, Category, Workouts_have_Exercises, Exercises_have_Equipment, Exercises_have_Categories, db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:brain5075@localhost/exerciseDB'
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
-def home():
-    cnx = mysql.connector.connect(user='root', password='brain5075', host='127.0.0.1', database='world')
-    
-    cursor = cnx.cursor()
-    # cursor = db_connect.connect_to_database()
-    
-    query = ('SELECT * FROM city')
-    cursor.execute(query)
-    
-    results = cursor.fetchall()
-    
-    cursor.close()
-    cnx.close()
-    
-    return render_template('index.html', data=results)
+def exercise_form():
+    return render_template('index.html')
 
+@app.route('/submit_exercise', methods=['POST'])
+def submit_exercise():
+    if request.method == 'POST':
+        data = request.get_json()
+        
+        #create new entries
+        exercise = Exercise(exercise_name=data['exercise_name'])
+        equipment = Equipment(equipment_name=data['equipment_name'])
+        workout = Workouts(workout_name=data['exercise_type'], calorie_count=data['calorie_count'])
+        
+        # many to many relationships
+        exercise.equipment.append(equipment)
+        workout.exercises.append(exercise)
+
+        db.session.add(exercise)
+        db.session.add(equipment)
+        db.session.add(workout)
+        db.session.commit()
+        
+        return jsonify({'message': 'Exercise data saved successfully'})
+        
 if __name__ == '__main__':
     app.run(port=3309, debug=True)
     
